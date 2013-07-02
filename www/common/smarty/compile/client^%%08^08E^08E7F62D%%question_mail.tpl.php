ecb332d540683e71932b602349829766b768e262
Константинов Дмитрий <poethrenoff@gmail.com>
2013-07-02 12:26:15 +0400

    Лишние файлы

--- www/common/smarty/compile/client^%%08^08E^08E7F62D%%question_mail.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.22, created on 2012-09-20 05:56:37
-         compiled from module/question/question_mail.tpl */ ?>
-<?php echo $this->_tpl_vars['question_author']; ?>
- (<?php echo $this->_tpl_vars['question_email']; ?>
-)
-
-<?php echo $this->_tpl_vars['question_content']; ?>
